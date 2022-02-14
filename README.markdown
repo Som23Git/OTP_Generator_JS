8ec5c73e6b7632165d82e4cc6e8e6cdf73278381
Som23Git <somesh.rokz@gmail.com>
2022-02-15 00:36:45 +0530

    OTP Generator Ver1.0

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# OTP Generator
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Som23/pen/VwrMepB](https://codepen.io/Som23/pen/VwrMepB).
+
+
---
+++ dist/script.js
@@ -0,0 +1,20 @@
+//OTP Generator Application
+
+document.write("Your OTP is \"" + otp_generator()+"\"");
+
+
+function otp_generator(){
+	var y = new Array(5);
+	sum = 0;
+	for(var i=0; i<6; i++){
+		var x = Math.random();
+		x = x*10;
+		y[i] = Math.floor(x);
+		console.log(y[i]);
+    if (y[i] == 0){
+       y[i] = 1;
+      }
+	}
+	y = y[0]*100000 + y[1]*10000 + y[2]*1000 + y[3]*100 + y[4]*10 + y[5]*1
+	return y;
+}
